f731fd78285deaa7388de13dfd0fdb1b7ea17611
yaakoveidelman <yeidelman@eidelmans.com>
2023-12-25 19:49:12 -0500

    #1

---
+++ canvas business spec.sql
@@ -0,0 +1,56 @@
+/*
+I'm running an image printing shop for posters and signs etc, and I'm getting overflowed with customers. 
+Until now I kept track of all the customer information with pen and paper, however there are way too many customers for me to keep track manually.
+I would like a software to help keep track of all the information. I hope you are able to help me out.
+Thank you.
+
+I need to keep track of the following information:
+The customers first name (First and middle together), the customers last name, canvas size, if the customer wants delivery, 
+if there is delivery the delivery fee, the product price, total price, if there is delivery we also need to know the customers address.
+
+This is all the information I need to keep track of for now.
+
+Q. What are the sizes of your canvases?
+A. We have four sizes, small, medium, large, and jumbo. Also note, we charge 50$ for small, 75$ for medium, 100$ for large and 150$ for jumbo.
+
+Q. What is your delivery fee?
+A. The delivery fee is 5% percent of the customers address number, i.e. 100 somewhere road is 5$.
+
+Q. Don't you also need to know the date the order was made, and the date the order got fulfilled?
+A. Good catch, yes.
+
+Q. Can a order by fulfilled the day it was ordered?
+A. No
+
+P.S. If you have any questions or are not sure of something, please feel free to reach out to me via slack (Yaakov Eidelman), any time of day or night, and I'll answer as soon as I can.
+
+
+I'll need the following reports.
+1. We are trying to improve our service speed, please show me the average time it takes for orders to be fulfilled. We need to know the speed it takes with delivery and without delivery separately.
+
+2. I need to keep track of how much revenue I make per month of each year. (Not Profit). It should be per month from the date ordered, as that is when we get paid.
+
+3. I would like to know which customers were happy. Show me which customers came back more than once.
+
+
+
+Sample Data
+Henry ford, medium, delivery, 748 Miles Street Holyoke, MA, 02/09/2021, 02/19/2021
+Lucy Dennis, small, delivery, 9345 East Road Mechanicsville, VA, 07/30/2023, 08/3/2023
+Lola Klein, small, delivery, 2 Pheasant Street Westerville, OH, 05/25/2018, 05/29/2018
+Erika Wilkins, jumbo, no delivery, 05/20/2005, 06/08/2005
+Cathy Montgomery, small, delivery, 7673 James St. Chesterton, IN, 08/21/2013, 08/28/2013
+Travis Austin, medium, no delivery, 09/09/2023, 09/022/2023
+Kerry Graves, large, no delivery, 10/23/2023, 10/28/2023
+Pat Rivera, large, delivery, 8642 Yule Street, Arvada CO, 12/06/2010, 12/10/2010
+Alexandra Estrada, small, no delivery, 02/21/2006, 02/22/2006
+Derek Jones, jumbo, delivery, 5331 Rexford Court, Montgomery AL, 04/18/2006, 04/21/2006
+Alexandra Estrada, small, no delivery, 04/11/2008, 04/15/2008
+Lola Klein, small, delivery, 2 Pheasant Street Westerville, OH, 06/03/2018, 06/07/2018
+Erika Wilkins, jumbo, no delivery, 011/12/2020, 11/20/2023
+Kerry Graves, large, no delivery, 03/03/2018, 03/09/2018
+Pat Rivera, large, delivery, 8642 Yule Street, Arvada CO, 07/07/2016, 07/12/2016
+Alexandra Estrada, small, no delivery, 02/04/2010, 02/15/2010
+*/
+
+
